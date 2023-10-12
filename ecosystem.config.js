192dacc4f1e02ad9f78c5d4bf3a4d6956eb57ee2
Everly Scott <everly@Everlys-MacBook-Pro.local>
2023-10-12 11:28:59 -0500

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
       path: "/var/TCA-Lighting",
       "pre-deploy-local": "",
       "post-deploy":
-        "pnpm install && ln ../config.json ./src/config.json && pnpm build && pm2 reload ecosystem.config.js",
+        "pnpm install && ln ../config.json ./src/config.json; pnpm build && pm2 reload ecosystem.config.js",
       "pre-setup": "",
     },
     productionRemote: {
